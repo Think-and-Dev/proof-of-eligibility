82c62c97ac989c81b6618c4bdbb59654b858b508
Lucas Marc <lucas@thinkanddev.com>
2025-11-22 18:12:12 -0300

    feat: added dotenv

--- dwn-server-plugin/scripts/run-dwn.js
+++ dwn-server-plugin/scripts/run-dwn.js
@@ -2,6 +2,9 @@ import { DwnServer } from '@web5/dwn-server';
 // node.js 18 and earlier, needs globalThis.crypto polyfill. needed for dwn-sdk-js
 import { webcrypto } from 'node:crypto';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 if (!globalThis.crypto) {
     // @ts-ignore
     globalThis.crypto = webcrypto;
