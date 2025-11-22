5520296fbc4cec11bc83bca8b3865e0cae4f8565
Lucas Marc <lucas@thinkanddev.com>
2025-11-22 16:31:56 -0300

    feat: added crypto polyfill for node 18 and earlier

--- dwn-server-plugin/scripts/run-dwn.js
+++ dwn-server-plugin/scripts/run-dwn.js
@@ -1,4 +1,11 @@
 import { DwnServer } from '@web5/dwn-server';
+// node.js 18 and earlier, needs globalThis.crypto polyfill. needed for dwn-sdk-js
+import { webcrypto } from 'node:crypto';
+
+if (!globalThis.crypto) {
+    // @ts-ignore
+    globalThis.crypto = webcrypto;
+}
 
 const dwnServer = new DwnServer();
 await dwnServer.start()
